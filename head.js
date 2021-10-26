4497e04181e69f7c3e32627b3356207b022feef0
RomanMedvedev91 <roman.medvedev77@gmail.com>
2021-10-26 00:13:34 +0000

    fix codding style

--- head.js
+++ head.js
@@ -11,11 +11,11 @@ const assertEqual = function(actual, expected) {
 
 const head = function(array) {
   return array[0];
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head([5]), 5);
-assertEqual(head([,6,7]), undefined);
+assertEqual(head([ ,6,7]), undefined);
 assertEqual(head([]), undefined);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([1, 33, 22]), "1");
