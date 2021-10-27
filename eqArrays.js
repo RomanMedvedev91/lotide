b9f323bf2d856ab0b3744d38537db10f941349c5
RomanMedvedev91 <roman.medvedev77@gmail.com>
2021-10-27 00:57:31 +0000

    create eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function(arr1, arr2) {
+  let output = false;
+  for(let i in arr1) {
+    if(arr1[i] !== arr2[i]) {
+      output = false;
+    } else {
+      output = true;
+    }
+  }
+  return output;
+}
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
