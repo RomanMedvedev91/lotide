95f5a8b734b344166af2af18ba08e42d961425d7
RomanMedvedev91 <roman.medvedev77@gmail.com>
2021-11-01 21:55:21 +0000

    refactor tail

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,29 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case 2: ...
-// ...
-const peers = tail(["Roman", "Steve", "Yo", "Ahmed"]);
-assertEqual(peers.length, 3);
-assertEqual(peers[0], "Steve");
-assertEqual(peers[2], "Ahmed");
-
-const empty = tail(["I will be sliced"]);
-assertEqual(empty.length, 0);
-assertEqual(empty[0], undefined);
+describe("#tail", () => {
+  it("returns 2 for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result, ["Lighthouse", "Labs"]);
+  });
+  it("returns ['Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[1], "Labs");
+  });
+  it("returns ['Steve', 'Yo', 'Ahmed'] for ['Roman', 'Steve', 'Yo', 'Ahmed']", () => {
+    const peers = tail(["Roman", "Steve", "Yo", "Ahmed"]);
+    assert.deepEqual(peers, ["Steve", "Yo", "Ahmed"]);
+  });
+  it("returns ['Steve'] for ['Roman', 'Steve', 'Yo', 'Ahmed']", () => {
+    const peers = tail(["Roman", "Steve", "Yo", "Ahmed"]);
+    assert.strictEqual(peers[0], "Steve");
+  });
+  it("returns ['Ahmed'] for ['Roman', 'Steve', 'Yo', 'Ahmed']", () => {
+    const peers = tail(["Roman", "Steve", "Yo", "Ahmed"]);
+    assert.strictEqual(peers[2], "Ahmed");
+  });
+});
